d2a99b57ffe5fd5241583f1552057104cb25572b
Daniel Chacón <id.chacon@uniandes.edu.co>
2021-12-07 18:15:24 -0500

    antigua carga de datos

--- App/config.py
+++ App/config.py
@@ -3,6 +3,6 @@ import sys
 file_path = os.path.join(os.path.dirname(__file__), '..')
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 sys.path.insert(0, os.path.abspath(file_path))
-data_airports = file_dir + '/Data/airports_full.csv'
-data_routes = file_dir + '/Data/routes_full.csv'
-data_worldcities = file_dir + '/Data/worldcities.csv'
+data_airports = file_dir + '/Data/airports-utf8-small.csv'
+data_routes = file_dir + '/Data/routes-utf8-small.csv'
+data_worldcities = file_dir + '/Data/worldcities-utf8.csv'
